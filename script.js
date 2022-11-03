c0842880e90b6e0a5cf164e17e710256cd0b95e8
Alex Zverev <alexzverev2448@gmail.com>
2022-11-03 17:46:22 +0300

    popup + script

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
-const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_profile');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__text');
 const profileFieldName = document.querySelector('.popup__field_name');
 const profileFieldAbout = document.querySelector('.popup__field_about');
+const profileForm = document.querySelector('.popup__container_profile');
 
 editButton.addEventListener('click', () => {
     profileFieldName.value = profileName.textContent
     profileFieldAbout.value = profileAbout.textContent
-    popup.classList.add('popup_opened')
+    popupProfile.classList.add('popup_opened')
 })
 
 closeButton.addEventListener('click', closePopup)
 
 function closePopup () {
-    popup.classList.remove('popup_opened')
+    popupProfile.classList.remove('popup_opened')
 }
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = profileFieldName.value
+    profileAbout.textContent = profileFieldAbout.value
+    closePopup()
+}
+
+profileForm.addEventListener('submit', formSubmitHandler);
